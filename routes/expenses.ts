2a05f8da8844eddb7193be7232c4297e8b40c9ef
Vraj Ved <vraj.a.ved@gmail.com>
2026-01-10 13:20:50 +0530

    Backend v1

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,24 +1,26 @@
 import { Hono } from "hono";
+import { zValidator } from '@hono/zod-validator'
 import z from "zod";
+import { fa } from "zod/locales";
 
-type Expense = {
-    id: number,
-    title: string,
-    amount: number
-}
+
 const fakeExpenses: Expense[] = [
     {id: 1, title: "Groceries", amount: 50}, 
     { id: 2, title: "Test", amount: 500 },
     { id: 3, title: "ABC", amount: 51210 }
 ]
 
+const expensesSchema = z.object({
+
+    id: z.number().int().positive().min(1),
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive()
+
+})
+
+type Expense = z.infer<typeof expensesSchema>;
 //  Our validation function
-const createPostSchema = z.object(
-    {
-        title: z.string(), 
-        amount: z.number()
-    }
-)
+const createPostSchema = expensesSchema.omit({id: true});
 
 export const expensesRoute = new Hono()
 
@@ -26,13 +28,40 @@ export const expensesRoute = new Hono()
     return c.json({expenses: fakeExpenses})
 })
 
-.post("/", async (c) => {
+.post("/", zValidator("json", createPostSchema), async (c) => {
 
-    const data = await c.req.json()
-    const expense = createPostSchema.parse(data)
+    const expense = await c.req.valid("json")
 
-    console.log(expense)
-    return c.json(expense)
+    const newExpense = {...expense, id: fakeExpenses.length + 1}
+    // insert at end using splice
+    fakeExpenses.splice(fakeExpenses.length, 0, newExpense)
+    console.log(newExpense)
+    c.status(201)
+    return c.json(newExpense)
 }) 
+.get("/:id{[0-9]+}", (c) => { 
+    // {[0-9]+} is regex to ensure only numbers are passed
+    const id = Number.parseInt(c.req.param("id"));
+    const expense = fakeExpenses.find((expense) => expense.id === id)
 
+    if (!expense) {
+        return c.notFound()
+    }
+
+    return c.json({expense});
+})
 // we need a validation function to check if expense type is same as the type we defined.
+
+
+.delete("/:id{[0-9]+}", (c) => { 
+    // {[0-9]+} is regex to ensure only numbers are passed
+    const id = Number.parseInt(c.req.param("id"));
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+
+    if (index === -1) {
+        return c.notFound();
+    }
+
+    const deletedExpense = fakeExpenses.splice(index, 1)[0];
+    return c.json({deletedExpense});
+})
